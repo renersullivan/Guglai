f7b2e5c07804a49839be2b4d06ce4793f8858d80
Felipe Ferreira Cabo <felipeferreirah@gmail.com>
2024-09-20 21:41:48 -0300

    subir modificações

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [],      
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  public escondeOuMostraResposta(event:any){
+    const divClicada  = event.target
+    const divAvo      = divClicada.parentElement.parentElement
+    const divTarget   = divAvo.getElementsByClassName('resposta')
+
+    divTarget[0].classList.toggle("esconde")
+  }
+
+
+
 }
